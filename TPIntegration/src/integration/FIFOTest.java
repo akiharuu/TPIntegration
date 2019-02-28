a00d173172c1c2d23baf6289f28c6600814ceab6
akiharuu <47241523+akiharuu@users.noreply.github.com>
2019-02-28 17:44:14 +0100

    Add files via upload

---
+++ TPIntegration/src/integration/FIFOTest.java
@@ -0,0 +1,44 @@
+package integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FIFOTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TPIntegration/src/integration/TriTableauTest.java
@@ -0,0 +1,34 @@
+package integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TriTableauTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testTrier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTriCroissant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTriDecroissant() {
+		fail("Not yet implemented");
+	}
+
+}
